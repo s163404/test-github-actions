493403963599088f2952fc0c3e37cd6bdff2e8ce
kkt-623 <s163404@gmail.com>
2026-02-15 17:33:27 +0900

    Add shortname to standings data extraction

--- src/scripts/update-standings.js
+++ src/scripts/update-standings.js
@@ -15,6 +15,7 @@ async function update() {
     const standings = data.standings[0].table.map(item => ({
       rank: item.position,
       name: item.team.name,
+      shortname: item.team.shortname,
       logo: item.team.crest,
       played: item.playedGames,
       wins: item.won,
